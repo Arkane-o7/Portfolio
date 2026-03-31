aee50f24566ee7a0a3b411955cb4e3a171f75242
Abhilaksh Chauhan <abhilaksh@StarLight.local>
2026-04-01 04:05:10 +0530

    Update portfolio

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -26,7 +26,10 @@ export default async function BlogsPage() {
           Blogs
         </Link>
         <Link href="/resume.pdf" target="_blank" rel="noopener noreferrer" className="nav-btn text-lg md:text-xl transition-all duration-300 text-[#BDBDBD] hover:text-white border-b border-transparent">
-          Resume ↗
+          Resume{' '}
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="inline-block w-4 h-4 ml-1 align-[-0.1em] transform -rotate-45">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+          </svg>
         </Link>
       </nav>
 
